1dd71ee94972b510e4f94fecd1bb8960289e43a8
InnaKolor1 <ineza.permiakova@yandex.ru>
2025-05-12 15:29:08 +0300

    commit2

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,52 +1,55 @@
 package org.skypro.skyshop;
 
 import org.skypro.skyshop.basket.ProductBasket;
-import org.skypro.skyshop.product.Product;
+import org.skypro.skyshop.product.DiscountedProduct;
+import org.skypro.skyshop.product.FixPriceProduct;
+import org.skypro.skyshop.product.SimpleProduct;
 
 public class App {
     public static void main(String[] args) {
         ProductBasket productBasket = new ProductBasket();
 
-        productBasket.addProduct(new Product("Молоко", 98));
-        productBasket.addProduct(new Product("Сахар", 54));
-        productBasket.addProduct(new Product("Яйца", 130));
+        productBasket.addProduct(new SimpleProduct("Молоко", 98));
+        productBasket.addProduct(new FixPriceProduct("Сахар"));
+        productBasket.addProduct(new DiscountedProduct("Яйца", 130,30));
+        printSeparator();
 
-        System.out.println("Добавить товар в полную корзину");
-        productBasket.addProduct(new Product("Кофе", 597));
-        line();
+        System.out.println("Добавить продукт в полную корзину");
+        productBasket.addProduct(new SimpleProduct("Кофе", 597));
+        printSeparator();
 
         System.out.println("Печать содержимого с несколькими продуктами");
         productBasket.printProductBasket();
-        line();
+        printSeparator();
 
         System.out.println("Сумма чека корзины с несколькими товарами");
         System.out.println("Окончательная стоимость: " + productBasket.getSumOfProducts());
-        line();
-
         System.out.println("Поиск товара в корзине");
         String name = "Молоко";
-        System.out.println(name + " - " + productBasket.checkProduct(name));
-        line();
+        System.out.println(name + " " + productBasket.checkProduct(name));
+        printSeparator();
 
         System.out.println("Распечатка содержимого перед очисткой");
         productBasket.printProductBasket();
         productBasket.cleanBasket();
-        line();
+        printSeparator();
+
+        System.out.println("Printing of the empty cart");
+        productBasket.printProductBasket();
+        printSeparator();
 
         System.out.println("Распечатка оценок перед очисткой");
         productBasket.printProductBasket();
-        line();
-
-        System.out.println("Цена пустой корзины");
-        System.out.println("\n" + "Цена пустых корзин: " + productBasket.getSumOfProducts());
-        line();
+        System.out.println("Цена пустой корзины: " + productBasket.getSumOfProducts());
+        printSeparator();
 
         System.out.println("Поиск товара в пустой корзине");
         name = "Сода";
-        System.out.println(name + " - " + productBasket.checkProduct(name));
+        System.out.println(name + " " + productBasket.checkProduct(name));
     }
 
-    public static void line() {
+    public static void printSeparator() {
+        System.out.println();
         System.out.println();
         System.out.println("---------------------------------------------------------------------------------");
     }

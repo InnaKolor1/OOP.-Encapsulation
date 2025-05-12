9c81121a7aa584d374f8f748d12c491f5727df9c
InnaKolor1 <ineza.permiakova@yandex.ru>
2025-05-12 14:03:31 +0300

    commit1

---
+++ src/Product/SimpleProduct.java
@@ -0,0 +1,45 @@
+package Product;
+
+import java.util.Objects;
+
+public class SimpleProduct extends Product {
+    private final double price;
+
+    public SimpleProduct(String name, double price) {
+        super(name, price);
+        if (price <= 0) {
+            throw new IllegalArgumentException(
+                    new StringBuilder("Цена продукции не может быть отрицательной или равна нулю.").toString()
+            );
+        }
+        this.price = price;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder()
+                .append(getName())
+                .append(": ")
+                .append(String.format("%.2f ₽", getPrice()))
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        SimpleProduct that = (SimpleProduct) o;
+        return Double.compare(that.price, price) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), price);
+    }
+}
